465df1e9d00e3af12fa5bacd22d5b7daadabdf9e
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2020-02-10 20:09:31 +0100

    Adds kill to running NPP to prevent borked installation
    Signed-off-by: Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,11 @@ maintainer        'Stefan Wessels Beljaars - Schuberg Philis'
 maintainer_email  'swesselsbeljaars@schubergphilis.com'
 license           'Apache-2.0'
 description       'Installs/Configures Notepad++'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.1.1'
+version           '1.1.2'
 
-chef_version      '>= 12.4' if respond_to?(:chef_version)
+chef_version      '>= 12.4'
 
-source_url        'https://github.com/schubergphilis/sbp_notepadplusplus' if respond_to?(:source_url)
-issues_url        'https://github.com/schubergphilis/sbp_notepadplusplus/issues' if respond_to?(:issues_url)
+source_url        'https://github.com/schubergphilis/sbp_notepadplusplus'
+issues_url        'https://github.com/schubergphilis/sbp_notepadplusplus/issues'
 
 supports          'windows'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,16 @@
 #
-# Cookbook Name:: sbp_notepadplusplus
+# Cookbook:: sbp_notepadplusplus
 # Attributes:: default
 #
-# Copyright 2020, Schuberg Philis B.V.
+# Copyright:: 2020, Schuberg Philis B.V.
 #
 #
+powershell_script 'Kill running notepad++' do
+  code 'Stop-Process -Name "notepad++" -Force'
+  ignore_failure true
+  action :nothing
+end
+
 if platform?('windows')
   windows_package 'Notepad++ (32-bit x86)' do
     source node['notepadplusplus']['url']
@@ -13,6 +19,7 @@ if platform?('windows')
     options '/S'
     version node['notepadplusplus']['version']
     action :install
+    notifies :run, 'powershell_script[Kill running notepad++]', :before
   end
 else
   Chef::Log.warn('Notepad++ can only be installed on the Windows platform.')
