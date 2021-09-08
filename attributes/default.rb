5f2bbbbaa3e4202d0c638b5bb001bb8035c6b986
Eric Kuiper <ekuiper@schubergphilis.com>
2021-09-08 16:47:08 +0200

    add x64 option and bump version

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,11 @@
 # Copyright:: 2020, Schuberg Philis B.V.
 #
 #
-default['notepadplusplus']['version'] = '7.8.4'
-default['notepadplusplus']['url'] = 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.4/npp.7.8.4.Installer.exe'
-default['notepadplusplus']['checksum'] = 'bda85bc0bb7beb11dbb9e9a964a2e2f0e4d35d0fc1e6b769e32b6847bfed8296'
+default['notepadplusplus']['x64']['install'] = false
+default['notepadplusplus']['x64']['version'] = '8.1.4'
+default['notepadplusplus']['x64']['url'] = 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.1.4/npp.8.1.4.Installer.x64.exe'
+default['notepadplusplus']['x64']['checksum'] = '27e6e296dbe7c5d5f069604c1f0f97a4d5e09f9c4bbb4e98f328ca0d77179e9b'
+
+default['notepadplusplus']['x86']['version'] = '8.1.4'
+default['notepadplusplus']['x86']['url'] = 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.1.4/npp.8.1.4.Installer.exe'
+default['notepadplusplus']['x86']['checksum'] = 'eadefd67e95bef200b2506caf8b3c82e0ea6c3cf895936614ba442989a08db4e'
