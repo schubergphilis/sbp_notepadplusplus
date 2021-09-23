02e0eedcf1963edb915865d6a40dd376dcf04238
Dennis de Goede <dedegoede@schubergphilis.com>
2021-09-23 17:00:19 +0200

    Add x64 option and bump Notepad++ version after testing with Timothy

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,22 +12,35 @@ powershell_script 'Kill running notepad++' do
 end
 
 if platform?('windows')
-  src_filename = node['notepadplusplus']['url'].split('/')[-1]
+  if node['notepadplusplus']['x64']['install']
+    package = 'Notepad++ (64-bit x64)'
+    url = node['notepadplusplus']['x64']['url']
+    checksum = node['notepadplusplus']['x64']['checksum']
+    version = node['notepadplusplus']['x64']['version']
+  else
+    package = 'Notepad++ (32-bit x86)'
+    url = node['notepadplusplus']['url']
+    checksum = node['notepadplusplus']['checksum']
+    version = node['notepadplusplus']['version']
+  end
+
+  src_filename = url.split('/')[-1]
   src_path = "#{Chef::Config['file_cache_path']}/#{src_filename}"
 
   remote_file src_path do
-    source node['notepadplusplus']['url']
-    checksum node['notepadplusplus']['checksum']
+    source url
+    checksum checksum
   end
 
-  windows_package 'Notepad++ (32-bit x86)' do
+  windows_package package do
     source src_path
     installer_type :custom
     options '/S'
-    version node['notepadplusplus']['version']
+    version version
     action :install
     notifies :run, 'powershell_script[Kill running notepad++]', :before
   end
+
 else
   Chef::Log.warn('Notepad++ can only be installed on the Windows platform.')
 end
